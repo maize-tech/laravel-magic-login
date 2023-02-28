ee0c97e0b5a9b0978c3bd0f95d98e09fcb6fce04
riccardodallavia <riccardo.dallavia@h-farm.com>
2023-02-28 10:05:00 +0100

    ADD method docs

--- src/Facades/MagicLink.php
+++ src/Facades/MagicLink.php
@@ -5,6 +5,9 @@ namespace Maize\MagicLogin\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static string make(\Illuminate\Contracts\Auth\Authenticatable $authenticatable, ?string $redirectUrl = null, \Illuminate\Support\Carbon|int|null $expiration = null, ?string $routeName = null, ?string $guard = null, ?int $loginsLimit = null, array $metadata = [], bool $notify = false)
+ * @method static string send(\Illuminate\Contracts\Auth\Authenticatable $authenticatable, ?string $redirectUrl = null, \Illuminate\Support\Carbon|int|null $expiration = null, ?string $routeName = null, ?string $guard = null, ?int $loginsLimit = null, array $metadata = [])
+ *
  * @see \Maize\MagicLogin\MagicLink
  */
 class MagicLink extends Facade
